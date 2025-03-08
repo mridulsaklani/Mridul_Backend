178897a8389015de9ddf6dfa4bd60005dfbc9c66
MRIDULSAKLANI <saklanimridul@gmail.com>
2025-03-08 20:59:08 +0530

    add

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const timelineRoute = require("./routes/timeline")
 require('dotenv').config();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000", 
+    origin: "*", 
     credentials: true,
   }));
 app.use(express.urlencoded({ extended: true }));
